a269775ef6628171c7319acbd923187764a4ee1f
vonwao <vonwao@gmail.com>
2020-09-07 09:45:45 -0400

    * add new control types to example * remove App.test.js because jest was finding it and giving error

--- example/src/schema.js
+++ example/src/schema.js
@@ -11,29 +11,47 @@ export const fields = [
     optionsKey: 'yes_no_option'
   },
   {
-    name: 'eggs',
+    name: 'egg_count',
     label: 'How many eggs?',
     validation: 'positive'
+  },
+  {
+    name: 'egg_type',
+    type: 'radioGroup',
+    label: 'How do you like your eggs?',
+    optionsKey: 'eggTypes'
+  },
+  {
+    name: 'egg_type2',
+    type: 'checkboxGroup',
+    label: 'How do you like your eggs 2?',
+    optionsKey: 'eggTypes'
+  },
+
+  {
+    name: 'eggs3',
+    fields: [
+      {
+        name: 'quantity'
+      },
+      {
+        name: 'scrambled',
+        type: 'select',
+        optionsKey: 'yes_no_option'
+      }
+    ]
   }
-  // {
-  //   name: 'eggs2',
-  //   fields: [
-  //     {
-  //       name: 'quantity'
-  //     },
-  //     {
-  //       name: 'scrambled',
-  //       type: 'select',
-  //       optionsKey: 'yes_no_option'
-  //     }
-  //   ]
-  // }
 ]
 
 export const options = {
   yes_no_option: [
     { value: true, label: 'Yes' },
     { value: false, label: 'No' }
+  ],
+  eggTypes: [
+    { value: 'S', label: 'Scrambled' },
+    { value: 'SSU', label: 'Sunny Side Up' },
+    { value: 'OE', label: 'Over Easy' }
   ]
 }
 
