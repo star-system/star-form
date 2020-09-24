8182adba3690236fc2164272f1cd8c0c59b9367e
vonwao <vonwao@gmail.com>
2020-09-24 13:17:12 -0400

    small improvements to demo

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { fields, options, validation } from './schema'
-import { useForm, FormContext, AutoForm } from '@star-js/form'
+import {
+  useForm,
+  FormContext,
+  AutoForm,
+  chakraComponentMap
+} from '@star-js/form'
 import { ThemeProvider } from 'emotion-theming'
 import { theme, Flex, Box, Button } from '@chakra-ui/core'
+import { SaveButton } from './SaveButton'
 
 const App = () => {
   // 1: Initialize form with hook
@@ -10,15 +16,17 @@ const App = () => {
     isEditing: true,
     fields,
     validation,
-    values: {}
+    values: {},
+    optionsMap: options,
+    controlMap: chakraComponentMap
   })
 
-  const { form } = autoForm
+  // const { form } = autoForm
 
   const save = async (form) => {
     // save data, for example
     // await fetch(`save_url`, {body: JSON.stringify(form) });
-    console.log(' form ', form)
+    console.log('SAVE form ', form)
   }
 
   return (
@@ -27,8 +35,9 @@ const App = () => {
         <Flex p={5}>
           <Box>
             <AutoForm fields={fields} options={options} />
-            <Button onClick={save}>Submit</Button>
-            <pre>{JSON.stringify(form)}</pre>
+            <SaveButton onSave={save} />
+            {/*<Button onClick={save}>Submit</Button>*/}
+            <pre>{JSON.stringify(autoForm.form)}</pre>
           </Box>
         </Flex>
       </FormContext.Provider>
